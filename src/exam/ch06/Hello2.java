94c1692c1ba23c9d508e771e247cfa7029a8ce77
NewWorld_ <32818386+INewWorldI@users.noreply.github.com>
2022-01-10 23:39:55 +0900

    Commit: java study 220110 (Method OverLoding)

---
+++ src/exam/ch06/Hello2.java
@@ -0,0 +1,8 @@
+package exam.ch06;
+
+// 하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
+
+public class Hello2 { } // 소스파일의 이름은 public class 이름과 일치해야함
+class Hello3 {} // 하나의 소스파일에는 하나의 public class 만 허용된다
+class Hello4 {} // public class 를 지정하지 않을경우 한파일에 존재하는 클래스 이름중에 어느걸 써도 상관없다
+
